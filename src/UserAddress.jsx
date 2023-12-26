bff702093f38378dd461031631ed0b7d6e3f421f
2joopiter <veryberrylike3@gmail.com>
2023-12-26 12:50:50 +0900

    리액트쿼리로 데이터 호출

--- src/UserAddress.jsx
+++ src/UserAddress.jsx
@@ -1,4 +1,7 @@
+import { useUserQuery } from './hooks/useUserQuery';
+
 export default function UserAddress() {
+	const result = useUserQuery();
 	return (
 		<div className='UserAddress'>
 			<h1>User Address</h1>
--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,4 +1,7 @@
+import { useUserQuery } from './hooks/useUserQuery';
+
 export default function UserName() {
+	const result = useUserQuery();
 	return (
 		<div className='UserName'>
 			<h1>User Name</h1>
