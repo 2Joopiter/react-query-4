02c16e98875f53cc753ae294b15ac03e1ce2a099
2joopiter <veryberrylike3@gmail.com>
2023-12-27 10:33:39 +0900

    stale, cacheTime을 관리하는 이유 및 차이점

--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,10 +1,16 @@
 import { useUserQuery } from './hooks/useUserQuery';
+import { useState } from 'react';
 
 export default function UserName() {
-	const result = useUserQuery(1);
+	const [Num, setNum] = useState(1);
+	const { isSuccess, data } = useUserQuery(Num);
+
 	return (
 		<div className='UserName'>
 			<h1>User Name</h1>
+			<button onClick={() => setNum(1)}>변경1</button>
+			<button onCLick={() => setNum(2)}>변경2</button>
+			<h2>name:{isSuccess && data.name}</h2>
 		</div>
 	);
 }
--- src/hooks/useUserQuery.js
+++ src/hooks/useUserQuery.js
@@ -1,20 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 
 const fetchUser = async ({ queryKey }) => {
-	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${queryKey[1]}`);
 	return await response.json();
 };
 
 export const useUserQuery = (num) => {
-  // useQuery에 첫번째 인수로 넣는 값은 무조건 fetching 함수로 전달됨.
-  // useQuery에는 쿼리키가 동일하면 같은 데이터라고 인지하기 때문에 refecthing 처리 안함
+	// useQuery에 첫번째 인수로 넣는 값은 무조건 fetching 함수로 전달됨.
+	// useQuery는 쿼리키가 동일하면 같은 데이터라고 인지하기 때문에 refecthing 처리 안함
 	return useQuery(['users', num], fetchUser, { refetchOnWindowFocus: false, refetchOnMount: false, cacheTime: 1000 * 5, staleTime: 1000 * 5 });
 };
 
 /*
-  fresh: 서버데이터를 최신으로 인식하는 상태 (refetch 필요없는 상태)
-  stale: 서버데이터를 오래된 상태로 인식하는 상태 (refetch 필요한 상태)
-  inactive: 서버데이터가 더이상 해당 컴포넌트에서 활용되지 않는 상태
-  cacheTime: inactive상태에서 얼마까지 데이터를 유지시킬지에 대한 시간값 (default: 1000 * 60 * 5ms 5분)
-  staleTime: 처음 dataFetching후 얼마뒤에 fresh -> stale로 변경할지에 대한 시간값 (default: 0ms)
+	refetchOnMount: 라우터만 이동하거나 하는 경우에는 재패칭이 일어나지 않고 캐치타임이 남아있으면 있는 캐시를 가져옴 
+	cacheTime: 해당 데이터를 컴포넌트에서 활용하지 않을 때, 얼마동안 캐시에서 삭제시키지 않을지에 대한 시간값 설정 (얼마동안 garbage collector에서의 데이터 삭제를 방지시킬지)(여기서 cache는 윈도우X react-query의 cache)
+	staleTime: 얼마동안 refetching을 하지 않을지에 대한 시간값 설정 (fresh 상태로 동결시킴). 이후 cacheTime이 없으면 데이터를 삭제.  
+
+	stealTime이 전부 소진되면 cacheTime으로 넘어가 소진
+	둘은 용법이 다름. stealTime은 refetching을 방지, cacheTime은 캐시 데이터 삭제 방지
 */
