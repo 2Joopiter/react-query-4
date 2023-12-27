773d9bae40db57820935b30d0e6accd7e090f691
2joopiter <veryberrylike3@gmail.com>
2023-12-27 11:49:48 +0900

    mutate 이용 기존 서버데이터 변경

--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,19 +1,24 @@
-import { useDeleteQuery, useUserQuery } from './hooks/useUserQuery';
+import { useState } from 'react';
+import { useDeleteQuery, useUpdateUser, useUserQuery } from './hooks/useUserQuery';
 
 export default function UserName() {
 	const { isSuccess, data } = useUserQuery();
-	const deleteUser = useDeleteQuery();
+	const updateUser = useUpdateUser();
+	const [UserName, setUserName] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		updateUser.mutate([UserName, 2]);
+	};
 
 	return (
 		<div className='UserName'>
 			<h1>User Name</h1>
-			<button
-				onClick={() => {
-					deleteUser.mutate(2);
-				}}
-			>
-				유저 데이터 삭제
-			</button>
+
+			<form onSubmit={handleSubmit}>
+				<input type='text' value={UserName || ''} onChange={(e) => setUserName(e.target.value)} />
+			</form>
+
 			<ul>{isSuccess ? data.map((user) => <li key={user.id}>{user.name}</li>) : <p>Loading...</p>}</ul>
 		</div>
 	);
--- src/hooks/useUserQuery.js
+++ src/hooks/useUserQuery.js
@@ -10,7 +10,32 @@ export const useUserQuery = () => {
 	return useQuery(['users'], fetchUser, { refetchOnWindowFocus: false, refetchOnMount: false, cacheTime: 1000 * 5, staleTime: 1000 * 5 });
 };
 
-//
+// 기존 서버 데이터 업데이트 함수
+export const updateUser = async (userName, num) => {
+	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${num}`, {
+		method: 'PATCH',
+		body: JSON.stringify({
+			name: userName,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	});
+	const result = await response.json();
+	return result;
+};
+
+// 데이터 변경 커스텀훅
+export const useUpdateUser = () => {
+	const queryClient = useQueryClient();
+
+	return useMutation(updateUser, {
+		onSuccess: (args) => {
+			queryClient.setQueryData(['users', args.id], args);
+		},
+	});
+};
+
 const deleteUser = async (num) => {
 	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${num[1]}`, {
 		method: 'DELETE',
@@ -20,21 +45,10 @@ const deleteUser = async (num) => {
 
 // 인수로 순번을 받아서 해당 순번의 데이터를 삭제하는 커스텀훅 (서버데이터 변경)
 export const useDeleteQuery = () => {
-	// 기존 생성한 queryClient 인스턴스를 호출하는 함수
-	// 해당 queryClient 인스턴스에서 활용할 수 있는 prototype method인 setQueryData라는 서버데이터 변경 요청값을 등록하는 함수를 가져옴(매서드를 쓰기 위해 가져오는 것)
 	const queryClient = useQueryClient();
-
-	// useMutation(비동기데이터 변경함수가 첫번째 인수, 옵션 설정 객체가 2번째 인수)
-	// 옵션 결정 객체: {onSuccess: mutate 요청이 성공적으로 수행되면 연결될 핸들러 함수}
-	// useMutation 훅이 delete user라는 내부 fetching함수를 호출해서 서버 데이터를 변경 요청함
-	return useMutation(
-		deleteUser,
-		// 서버데이터 변경이 성공시 변경된 서버데이터값을 받아서 다시 고유의 쿼리키로 등록해서 react-query가 관리할 수 있게 함
-		// 변경된 데이터를 args로 담아서, args.id라는 새로운 쿼리키에 등록
-		{
-			onSuccess: (args) => {
-				queryClient.setQueryData(['users', args.id], args);
-			},
-		}
-	);
+	return useMutation(deleteUser, {
+		onSuccess: (args) => {
+			queryClient.setQueryData(['users', args.id], args);
+		},
+	});
 };
