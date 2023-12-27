c4a96afcc6b40ae1495f774b17cc6a464771e415
2joopiter <veryberrylike3@gmail.com>
2023-12-27 11:04:45 +0900

    삭제 전용 커스텀훅 추가

--- src/hooks/useUserQuery.js
+++ src/hooks/useUserQuery.js
@@ -1,21 +1,40 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 const fetchUser = async ({ queryKey }) => {
-	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${queryKey[1]}`);
+	const response = await fetch(`https://jsonplaceholder.typicode.com/users/`);
 	return await response.json();
 };
 
-export const useUserQuery = (num) => {
-	// useQuery에 첫번째 인수로 넣는 값은 무조건 fetching 함수로 전달됨.
-	// useQuery는 쿼리키가 동일하면 같은 데이터라고 인지하기 때문에 refecthing 처리 안함
-	return useQuery(['users', num], fetchUser, { refetchOnWindowFocus: false, refetchOnMount: false, cacheTime: 1000 * 5, staleTime: 1000 * 5 });
+// 데이터 목록 호출 커스텀훅
+export const useUserQuery = () => {
+	return useQuery(['users'], fetchUser, { refetchOnWindowFocus: false, refetchOnMount: false, cacheTime: 1000 * 5, staleTime: 1000 * 5 });
 };
 
-/*
-	refetchOnMount: 라우터만 이동하거나 하는 경우에는 재패칭이 일어나지 않고 캐치타임이 남아있으면 있는 캐시를 가져옴 
-	cacheTime: 해당 데이터를 컴포넌트에서 활용하지 않을 때, 얼마동안 캐시에서 삭제시키지 않을지에 대한 시간값 설정 (얼마동안 garbage collector에서의 데이터 삭제를 방지시킬지)(여기서 cache는 윈도우X react-query의 cache)
-	staleTime: 얼마동안 refetching을 하지 않을지에 대한 시간값 설정 (fresh 상태로 동결시킴). 이후 cacheTime이 없으면 데이터를 삭제.  
+//
+const deleteUser = async ({ queryKey }) => {
+	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${queryKey[1]}`, {
+		method: 'DELETE',
+	});
+	return await response.json();
+};
 
-	stealTime이 전부 소진되면 cacheTime으로 넘어가 소진
-	둘은 용법이 다름. stealTime은 refetching을 방지, cacheTime은 캐시 데이터 삭제 방지
-*/
+// 인수로 순번을 받아서 해당 순번의 데이터를 삭제하는 커스텀훅 (서버데이터 변경)
+export const useDeleteQuery = () => {
+	// 기존 생성한 queryClient 인스턴스를 호출하는 함수
+	// 해당 queryClient 인스턴스에서 활용할 수 있는 prototype method인 setQueryData라는 서버데이터 변경 요청값을 등록하는 함수를 가져옴(매서드를 쓰기 위해 가져오는 것)
+	const queryClient = useQueryClient();
+
+	// useMutation(비동기데이터 변경함수가 첫번째 인수, 옵션 설정 객체가 2번째 인수)
+	// 옵션 결정 객체: {onSuccess: mutate 요청이 성공적으로 수행되면 연결될 핸들러 함수}
+	// useMutation 훅이 delete user라는 내부 fetching함수를 호출해서 서버 데이터를 변경 요청함
+	return useMutation(
+		deleteUser,
+		// 서버데이터 변경이 성공시 변경된 서버데이터값을 받아서 다시 고유의 쿼리키로 등록해서 react-query가 관리할 수 있게 함
+		// 변경된 데이터를 args로 담아서, args.id라는 새로운 쿼리키에 등록
+		{
+			onSuccess: (args) => {
+				queryClient.setQueryData(['users', args.id], args);
+			},
+		}
+	);
+};
